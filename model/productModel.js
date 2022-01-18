4c3a5bd053886609a91b6f7cfa7d2f985327bfd4
prabhat-cn <prabhat@capitalnumbers.com>
2022-01-18 20:14:04 +0530

    ⚡️

--- model/productModel.js
+++ model/productModel.js
@@ -1,9 +1,33 @@
 const mongoose = require('mongoose');
+const { stringify } = require('nodemon/lib/utils');
 
-const productSchema = new mongoose.Schema({
-  title: String,
-  price: String,
-  image: String,
-  details: String,
-});
+const productSchema = new mongoose.Schema(
+  {
+    title: String,
+    price: String,
+    image: String,
+    details: String,
+  },
+  { timestamps: true }
+);
 module.exports = mongoose.model('Product', productSchema);
+
+// const productSchema = new mongoose.Schema(
+//   {
+//     title: {
+//       type: String,
+//     },
+//     price: {
+//       type: String,
+//     },
+//     image: {
+//       type: String,
+//     },
+//     details: {
+//       type: String,
+//     },
+//   },
+//   { timestamps: true }
+// );
+// const Products = mongoose.model('Product', productSchema);
+// module.exports = Products;
